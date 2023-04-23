686ce7e12f49f85914c65d239176397bd505bf7d
ArturS2307 <art.salakhutdinov@gmail.com>
2023-04-23 13:42:50 +0300

    update fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -11,13 +11,10 @@ class ApiRestCountries {
 
     fetchCountries() {
         const url = 'https://restcountries.com/v3.1/name/';
-        return fetch(`${url}${this.searchQuery}?fields=name,capital,population,flags,languages`)
-        .then(response => {
+        return fetch(`${url}${this.searchQuery}?fields=name,capital,population,flags,languages`).then(response => {
             if(!response.ok) {throw new Error(response.status);
             return}
-            return response.json()})
-        .then(data => {console.log(data); return data})
-        .catch((error) => Notiflix.Notify.failure('Oops, there is no country with that name'))
+            return response.json()}).then(data => {console.log(data); return data}).catch((error) => Notiflix.Notify.failure('Oops, there is no country with that name'));
     };
 
     get query() {
